c0f2efc9f7d35fcc8b92b92a11f7fec27f67191d
northlanee <iwasmusic16@gmail.com>
2020-11-18 15:36:37 +0200

    copy to clipboard added

--- src/components/BonusList/Bonus/Bonus.tsx
+++ src/components/BonusList/Bonus/Bonus.tsx
@@ -3,6 +3,7 @@ import React, { FC, ReactElement } from "react";
 import { Bonus as BonusType } from "sharedTypes";
 
 import "./Bonus.styles.scss";
+import copy from "assets/images/copy.svg";
 
 type PropTypes = {
   bonus: BonusType;
@@ -12,7 +13,12 @@ type PropTypes = {
 const Bonus: FC<PropTypes> = React.memo(
   ({ bonus, activateBonusHandler }): ReactElement => {
     const { title, description, link, promocode, activated } = bonus;
-    console.log("bonus " + title + " rendered");
+    const inputEl = React.useRef() as React.MutableRefObject<HTMLInputElement>;
+
+    const copyToClipboard = (e: React.MouseEvent<HTMLButtonElement>) => {
+      inputEl.current.select();
+      document.execCommand("copy");
+    };
 
     return (
       <div className="bonus">
@@ -25,12 +31,19 @@ const Bonus: FC<PropTypes> = React.memo(
         <div className="bonus__promocode">
           <span className="bonus__promocode-title">Promocode</span>
           <div className="bonus__promocode-actions">
-            <input
-              type="text"
-              className="bonus__promocode-input"
-              value={promocode}
-              readOnly
-            />
+            <div className="bonus__promocode-wrapper">
+              <input
+                type="text"
+                className="bonus__promocode-input"
+                value={promocode}
+                readOnly
+                ref={inputEl}
+              />
+              <span onClick={copyToClipboard} className="bonus__promocode-copy">
+                <img src={copy} alt="copy" />
+              </span>
+            </div>
+
             <button
               className="bonus__promocode-button"
               onClick={() => activateBonusHandler(title)}
